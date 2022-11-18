417ae2b85883aedb75a8735b2d86f5923c425fd8
Elvis <elviscruz45@gmail.com>
2022-11-18 14:21:56 -0500

    class27 end

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react"
+import {Link} from "react-router-dom"
+import "../styles/components/Header.css"
 
 const Header=()=>(
     <div className="Header">
-        <h1 className="Header-title">PlatziConf Merch</h1>
+            <h1 className="Header-title">
+                <Link to="/">
+                PlatziConf Merch
+                </Link>
+                
+            </h1>
         <div className="Header-checkout">
-            Checkout, pruebas
+            <Link to="/checkout">
+                <i className="fas fa-shopping-basket"></i>
+            </Link>
         </div>
     </div>)
 
 
 export default Header
+
