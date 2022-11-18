55977335acbbb3aac3400d4a90cf630f3f14fb7b
Elvis <elviscruz45@gmail.com>
2022-11-18 13:02:39 -0500

    class25 finish

--- src/container/Success.jsx
+++ src/container/Success.jsx
@@ -1,9 +1,20 @@
 import React from "react"
+import "../styles/components/Success.css"
 
 const Success=()=>{
     return(
-        <h1> Success</h1>
+        <div className="Success">
+            <div className="Success-content">
+                <h2> Elvis, Gracias por tu compra</h2>
+                <span>Tu pedido llegara en 3 dias a tu direccion: </span>
+                <div className="Sucess-map">
+                    Google Maps
+                </div>
+            </div>
+        </div>
+
     )   
 }
 
 export default Success
+
