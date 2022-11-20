fa38830f961eb9dd8be38769bb555b1215556885
Elvis <elviscruz45@gmail.com>
2022-11-20 18:39:50 -0500

    finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
-import React from "react"
+import React , {useContext} from "react"
 import {Link} from "react-router-dom"
 import "../styles/components/Header.css"
+import AppContext from "../context/AppContext"
 
-const Header=()=>(
+
+const Header=()=>{
+    const {state}=useContext(AppContext)
+    const {cart}=state
+
+return(
     <div className="Header">
             <h1 className="Header-title">
                 <Link to="/">
@@ -14,8 +20,12 @@ const Header=()=>(
             <Link to="/checkout">
                 <i className="fas fa-shopping-basket"></i>
             </Link>
+            {cart.length>0 && <div className="Header-alert">{cart.length}</div>}
         </div>
-    </div>)
+    </div>
+)
+    
+}
 
 
 export default Header
